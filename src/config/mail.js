7cf6001b27b89fed8e59f8e6652321710e52e015
willian emboava <willian@Willians-MacBook-Pro-2.local>
2019-06-30 22:50:15 -0300

    send email config

---
+++ src/config/mail.js
@@ -0,0 +1,18 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'ae5efe0f709956',
+    pass: '5843ad0e05ab67',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// sparkpost
+
+// mailtrap -> somente para dev
