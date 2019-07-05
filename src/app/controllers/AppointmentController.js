92beff875c53269acffc8a651cbeb1e2bbf75eb5
willian emboava <willian@Willians-MacBook-Pro-2.local>
2019-07-05 01:49:27 -0300

    added redis and bee queue for email queue

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,10 +4,10 @@ import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
-
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -152,17 +152,9 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h.'", {
-          locale: pt,
-        }),
-      },
+    // send email
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
